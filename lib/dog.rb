72f8a8d31e955c5245c79a9a5e5e969c451e85e8
Jane Venter <janeanneventer@gmail.com>
2019-01-31 20:08:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -94,7 +94,8 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?", name, breed)
     
-    if !song = empty 
+    if !song.empty?
+      dog_data = dog[0]
       
   end
 end
