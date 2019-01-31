3dca73898f377c0d1d3501c737aaa623b9609ad6
Jane Venter <janeanneventer@gmail.com>
2019-01-31 19:17:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,11 @@ class Dog
     dog
   end
   
-  def self.find_by_name 
-    
+  def self.find_by_name(name) 
+    sql = <<-SQL
+      SELECT *
+      FROM dogs 
+      WHERE 
   end
   
   def self.create(name:, breed:)
