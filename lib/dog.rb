9f26ccb6a80fa82896a909b51b56554f4adc5cb2
Jane Venter <janeanneventer@gmail.com>
2019-01-31 18:25:59 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  attr_accessor :id, :name, :breed
+  
+  def initialize(id: nil, name:, breed:)
+    @id = id 
+    @name = name 
+    @breed = breed
+  end
+  
+  def .create_table 
+    sql = >>-SQL
+      CREATE TABLE dogs 
+      (id INTEGER PRIMARY KEY, name TEXT, breed TEXT);
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+end
