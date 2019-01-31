e3aa0719df2dae69878579478bf574b35c982f40
Jane Venter <janeanneventer@gmail.com>
2019-01-31 18:28:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,9 +11,9 @@ class Dog
   def .create_table 
     sql = >>-SQL
       CREATE TABLE dogs (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        breed TEXT
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      breed TEXT
         );
     SQL
     
