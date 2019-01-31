0fbfdb14e2383bad1378fef4cf6f17d12521fc29
Jane Venter <janeanneventer@gmail.com>
2019-01-31 19:10:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,7 +44,10 @@ class Dog
   end
   
   def self.new_from_db(row)
-    dog = self.new(row[0], row[1], row[2])
+    dog = self.new()
+    dog.id = row[0]
+    dog.name = row[1]
+    dog.breed = row[2]
     dog
   end
   
